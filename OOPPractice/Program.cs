c9c2b207873d36d794759cfbc3221d7c6a1d0ebb
MarthaKent810 <martybacho@gmail.com>
2018-05-31 08:22:56 -0400

    first commit

---
+++ OOPPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Dog wooffie = new Dog(15, 120, 900, 0);
+            wooffie.Run();
+            Dog spot = new Dog();
+            spot.RunningSpeed = 25;
+            spot.Run();
+            wooffie.RunningSpeed = 100;
+            wooffie.Run();
+            spot.Run();
+        }
+    }
+}
+
